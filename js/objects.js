24a40be5ce76a8fff9421551f4574be3470c4f0a
rtroy <rtroyh@gather.cl>
2017-02-03 21:11:19 -0300

    EJEMPLOS DE OBJETOS

--- js/objects.js
+++ js/objects.js
@@ -19,3 +19,16 @@ var flight = {
         city: "Los Angeles"
     }
 };
+
+if (typeof Object.create !== 'function') {
+    Object.create = function (o) {
+        var F = function () {
+        };
+
+        F.prototype = o;
+
+        return new F();
+    };
+}
+
+var another_stooge = Object.create(stooge);
