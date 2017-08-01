bc778725498a3a09557f3ae53a6309dd53a9c634
rtroy <rtroyh@gather.cl>
2017-08-01 18:46:22 -0400

    EJEMPLO DE EXTENSION DE UN OBJETO

---
+++ augment/augment.js
@@ -0,0 +1,39 @@
+Function.prototype.agregarMetodo = function (name, func) {
+    if (!this.prototype[name]) {
+        console.log(this);
+        console.log("Nombre función: " + name);
+        console.log("Definicion Función: " + func);
+        this.prototype[name] = func;
+    }
+
+    return this;
+};
+
+Number.agregarMetodo('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+
+Number.agregarMetodo('lala', this);
+
+console.log("lala");
+console.log((1).lala);
+
+console.log((10 / 3).integer());
+
+
+var Persona = function (nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+};
+
+var persona1 = new Persona('juan', 'Perez');
+console.log(persona1);
+
+Persona.prototype.imprimete = function () {
+    console.log("Nombre:" + this.nombre);
+    console.log("Apellido: " + this.apellido);
+    return this;
+};
+
+console.log(persona1.imprimete());
