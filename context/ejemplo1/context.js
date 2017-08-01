897b54d42fb84e7f4da01a56986289f03d400acb
rtroy <rtroyh@gather.cl>
2017-08-01 17:43:28 -0400

    MAS EJEMPLOS PARA CONTEXT

--- context/ejemplo1/context.js
+++ context/ejemplo1/context.js
@@ -7,14 +7,20 @@ console.log("var a = 1;");
 console.log("¿es window.a === this.a?");
 console.log(window.a === this.a);
 
+
 console.log("\n¿Y adentro de una función que sera 'this'?");
+console.log("function unaFuncion(){console.log(this)}");
+console.log("unaFuncion()");
+
 function unaFuncion() {
     console.log(this);
 }
-unaFuncion();
-console.log("Tambien es 'windows', porque unaFuncion las declaraciones en la raiz quedan montadas en el objeto window");
 
-console.log("\n¿Y adentro de un objeto?");
+unaFuncion();
+console.log("Tambien es 'windows', porque 'unaFuncion' esta vinculada al objeto window");
+
+
+console.log("\n¿Y en el metodo de un objeto?");
 var unObject = {
     a: 1,
     unaFuncion: function () {
@@ -22,14 +28,60 @@ var unObject = {
     }
 };
 unObject.unaFuncion();
-console.log("Como la funcion esta montada sobre el objeto 'unObject', this devuelve 'unObject'");
+console.log("Como la función esta vinculada al objeto 'unObject', 'this' devuelve 'unObject'");
 
-console.log("\nAunque le puedo pasar el context usando 'call' :)");
+
+console.log("\nAunque le puedo pasar el context usando 'call'");
 unObject.unaFuncion.call(window);
 
-unObject = {
+
+var myClick = function () {
+    console.log("\n¿En un evento?");
+    console.log(this);
+};
+
+console.log("\n¿Y en un metodo definido dentro de otro metodo?");
+var otroObjeto = {
     a: 1,
     unaFuncion: function () {
-        console.log(this);
+        var that = this;
+        var funcionDentroDeOtraFuncion = function () {
+            console.log(this);
+            console.log(that);
+        };
+
+        funcionDentroDeOtraFuncion();
     }
 };
+otroObjeto.unaFuncion();
+console.log("Se debe usar una variable (var that) para poder acceder al objeto.");
+
+
+var objetosAnidados = {
+    funcion0: function () {
+        console.log(this);
+    },
+    objetoAnidado1: {
+        funcion1: function () {
+            console.log(this);
+        },
+        funcion11: function () {
+            var funcionMuyAnidada = function () {
+                var funcionMasAnidadaAun = function () {
+                    console.log(this);
+                };
+
+                console.log(this);
+                funcionMasAnidadaAun();
+            };
+
+            funcionMuyAnidada();
+            console.log(this);
+        }
+    }
+};
+
+objetosAnidados.funcion0();
+objetosAnidados.objetoAnidado1.funcion1();
+objetosAnidados.objetoAnidado1.funcion11();
+
