0a997f4ae7eff72172b7ab95479984f23be31f6b
rtroy <rtroyh@gather.cl>
2017-08-29 13:29:31 -0300

    NUEVA DOCUMENTACION

--- IIFE/IIFE.js
+++ IIFE/IIFE.js
@@ -1,7 +1,22 @@
+
+(function () {
+    var a = 1;
+    console.log("Esto es una Immediately-Invoked Function Expression");
+
+    console.log(this);
+
+    console.log(this === window);
+
+    console.log(a);
+
+    console.log(this.a === window.a);
+})();
+
+
 $(function () {
     var a2 = 1;
 
-    console.log("\nCon jquery");
+    console.log("\nCon jquery:");
 
     console.log(this);
 
@@ -11,15 +26,3 @@ $(function () {
 
     console.log(this.a2 === document.a2);
 });
-
-(function () {
-    var a = 1;
-    console.log("Esto es una Immediately-Invoked Function Expression");
-
-    console.log(this);
-
-    console.log(this === window);
-    console.log(a);
-
-    console.log(this.a === window.a);
-})();
