50ded27a6b5eb2eff81999ea48589e6cc890e702
rtroy <rtroyh@gather.cl>
2017-09-04 16:30:26 -0300

    ACTUALIZACION EJEMPLO "CONTEXT"

--- context/ejemplo3/assert.js
+++ context/ejemplo3/assert.js
@@ -1,5 +1,7 @@
 function assert(value,
                 text) {
+    console.log(text);
+
     var li = document.createElement("li");
     li.className = value ? "pass" : "fail";
     li.appendChild(document.createTextNode(text));
