6eaee711c04b567774053bd0c9969f0f62c04720
rodrigotroy <rtroyh@gather.cl>
2022-01-23 12:44:30 -0300

    Nuevos ejemplos

---
+++ class/example1.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6 */
+
+/* global console*/
+class X {
+    get Y() {
+        return 10;
+    }
+
+    get name() {
+        return "I am X";
+    }
+}
+
+const x = new X();
+console.log(x.y);
+console.log(x.name);
+
+let y = [1, 2];
+console.log(y.length);
+y[100] = 10;
+console.log(y.length);
+
+var start = 1;
+
+function setEnd() {
+    var end = 10;
+}
+
+setEnd();
+console.log(end);
+
---
+++ class/example2.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+
+/* global console*/
+
+const x = [1, 2];
+const y = [5, 7];
+const z = [...x, ...y];
+console.log(z);
