d0f3041625ba900fee5b726834c66e8d358ac92a
rodrigotroy <rodrigotroy@me.com>
2022-02-01 21:59:36 -0300

    new async/await example

---
+++ async/example2/app.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 8 */
+
+/* global console*/
+const brainstormDinner = require('./library.js');
+
+function nativePromiseDinner() {
+    "use strict";
+    brainstormDinner()
+        .then((meal) => {
+            console.log(`I'm going to make ${meal} for dinner.`);
+        });
+}
+
+
+// async/await version:
+async function announceDinner() {
+    "use strict";
+    return await brainstormDinner()
+        .then((meal) => {
+            console.log(`I'm going to make ${meal} for dinner.`);
+        });
+}
+
+let promise = announceDinner();
+console.log("Thinking about it...");
+promise.then(() => {
+    "use strict";
+    console.log("Done!");
+});
+
+
