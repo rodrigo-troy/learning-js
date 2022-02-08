47f5bb9bdc1d6da8c973accc2cf35aab9a97faf7
rodrigotroy <rtroyh@gather.cl>
2022-02-08 17:34:10 -0300

    new examples

---
+++ fetch/example2/app.js
@@ -0,0 +1,112 @@
+/*jshint esversion: 8 */
+
+/* global console*/
+
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+const responseField = document.querySelector('#responseField');
+const jsonResponseField = document.querySelector('#jsonResponseField');
+const rawResponseField = document.querySelector('#rawResponseField');
+
+const renderRawResponse = (res) => {
+    "use strict";
+    // Takes the first 10 words from res
+    let trimmedResponse = res.slice(0,
+                                    10);
+    // Manipulates responseField to render the unformatted response
+    rawResponseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
+
+    return res;
+};
+
+const renderResponse = (res) => {
+    "use strict";
+    // Handles if res is falsey
+    if (!res) {
+        console.log(res.status);
+    }
+    // In case res comes back as a blank array
+    if (!res.length) {
+        responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
+        return;
+    }
+
+    // Creates an empty array to contain the HTML strings
+    let wordList = [];
+    // Loops through the response and caps off at 10
+    for (let i = 0; i < Math.min(res.length,
+                                 10); i++) {
+        // creating a list of words
+        wordList.push(`<li>${res[i].word}</li>`);
+    }
+    // Joins the array of HTML strings into one string
+    wordList = wordList.join("");
+
+    // Manipulates responseField to render the modified response
+    responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
+
+    return res;
+};
+
+const renderJsonResponse = (res) => {
+    "use strict";
+    // Creates an empty object to store the JSON in key-value pairs
+    let rawJson = {};
+    for (let key in res) {
+        rawJson[key] = res[key];
+    }
+    // Converts JSON into a string and adding line breaks to make it easier to read
+    rawJson = JSON.stringify(rawJson)
+                  .replace(/,/g,
+                           ", \n");
+    // Manipulates responseField to show the returned JSON.
+    jsonResponseField.innerHTML = `<pre>${rawJson}</pre>`;
+
+    return res;
+};
+
+const getSuggestions = async () => {
+    "use strict";
+    const wordQuery = 'dog';
+    const endpoint = `${url}${queryParams}${wordQuery}`;
+
+    try {
+        const response = await fetch(endpoint,
+                                     {cache: 'no-cache'});
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderRawResponse(jsonResponse);
+            renderResponse(jsonResponse);
+            renderJsonResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    /*fetch(endpoint,
+          {cache: 'no-cache'})
+        .then(response => {
+                  console.log(response);
+
+                  if (response.ok) {
+                      return response.json();
+                  }
+
+                  throw new Error('Request failed!');
+              },
+              networkError => {
+                  console.log(networkError.message);
+              })
+        .then(jsonResponse => {
+            return renderRawResponse(jsonResponse);
+        })
+        .then(jsonResponse => {
+            return renderResponse(jsonResponse);
+        })
+        .then(jsonResponse => {
+            renderJsonResponse(jsonResponse);
+        });*/
+};
+
+getSuggestions();
