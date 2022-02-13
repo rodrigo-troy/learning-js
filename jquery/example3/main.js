a457c189b714daf81440a69c016b9e575b8cadc0
rodrigotroy <rodrigotroy@me.com>
2022-02-13 17:27:38 -0300

    new examples

--- jquery/example3/main.js
+++ jquery/example3/main.js
@@ -1,3 +1,6 @@
+/*jshint esversion: 6 */
+
+/* global console*/
 $(document).ready(() => {
     $('.hint-box').on('click',
                       () => {
@@ -8,23 +11,41 @@ $(document).ready(() => {
                               () => {
                                   $('.wrong-text-one').fadeOut('fast');
                                   $('.frown').show();
+                                  $('.smiley').hide();
                               });
 
     $('.wrong-answer-two').on('click',
                               () => {
                                   $('.wrong-text-two').fadeOut('fast');
                                   $('.frown').show();
+                                  $('.smiley').hide();
                               });
 
     $('.wrong-answer-three').on('click',
                                 () => {
                                     $('.wrong-text-three').fadeOut('fast');
                                     $('.frown').show();
+                                    $('.smiley').hide();
                                 });
 
     $('.correct-answer').on('click',
                             () => {
                                 $('.frown').fadeOut('fast');
-                                $('.smiley').show();
+                            })
+                        .on('click',
+                            () => {
+                                setTimeout(() => {
+                                               $('.smiley').fadeIn(500);
+                                           },
+                                           500);
                             });
+
+    $('div').on('mouseenter',
+                (event) => {
+                    $(event.currentTarget).addClass('borderRed');
+                })
+            .on('mouseleave',
+                (event) => {
+                    $(event.currentTarget).removeClass('borderRed');
+                });
 });
