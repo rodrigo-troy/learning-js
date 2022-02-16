04e8ece1ff07b07783be9acf7d497952dd5c9601
rodrigotroy <rodrigotroy@me.com>
2022-02-16 20:52:00 -0300

    new examples

--- algorithm/recursive-sum.js
+++ algorithm/recursive-sum.js
@@ -1,12 +1,11 @@
-/*jshint esversion: 8 */
-
-/* global console*/
 function sum(n) {
     if (n === 1) {
+        console.log('sum(1) = 1');
         return 1;
     } else {
+        console.log(`sum(${n - 1}) + ${n}`);
         return sum(n - 1) + n;
     }
 }
 
-console.log(sum(3));
+console.log(sum(5));
