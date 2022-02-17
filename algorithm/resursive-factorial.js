5693b4e3c6d91a561974897ea4f0e7440457744c
rodrigotroy <rodrigotroy@me.com>
2022-02-17 19:13:45 -0300

    new examples

---
+++ algorithm/resursive-factorial.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+    if (n === 0) {
+        console.log('factorial(0) = 1');
+        return 1;
+    } else {
+        console.log(`factorial(${n}) = factorial(${n - 1}) * ${n}`);
+        return factorial(n - 1) * n;
+    }
+}
+
+console.log(factorial(1));
